6f7d970b42f89c18401570c6fc2a3f0fe07e4611
Toni Daada <tonidaadaadagmail.com>
2024-11-20 13:36:29 +0100

    modified:   script.js

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 const account1 = {
-  owner: 'Jonas Schmedtmann',
+  owner: 'Sophia Umukoro',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
+  owner: 'Toni Daada',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
 };
 
 const account3 = {
-  owner: 'Steven Thomas Williams',
+  owner: 'Victory Arogundade',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
 };
 
 const account4 = {
-  owner: 'Sarah Smith',
+  owner: 'Ogunyemi Subomi',
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
@@ -78,3 +78,15 @@ const displayMovements = function (movements) {
 };
 
 displayMovements(account1.movements);
+
+const createUserName = function (account) {
+  account.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(value => value.at(0))
+      .join('');
+  });
+};
+
+createUserName(accounts);
